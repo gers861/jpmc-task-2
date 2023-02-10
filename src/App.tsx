68f96b7af5285ff97285b547a37a87f2f53f2df5
gers861 <gers861@gmail.com>
2023-02-10 19:58:29 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ class App extends Component<{}, IState> {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
+      this.setState({ data:serverResponds,
     });
   });
   x++
